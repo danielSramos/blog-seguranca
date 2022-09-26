2b6c708251cd95df0e8dd720902eced2a25ae340
Daniel Ramos <danieldsrce@gmail.com>
2022-09-26 16:56:02 -0300

    implementação de autenticação via JWT

---
+++ src/usuarios/estrategias-autenticacao.js
@@ -0,0 +1,39 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const Usuario = require('./usuarios-modelo');
+const { InvalidArgumentError } = require('../erros')
+
+const bcrypt = require('bcrypt');
+
+
+function verificaUsuario(usuario) {
+    if (!usuario) {
+        throw new InvalidArgumentError('Usuário não existe');
+    }
+}
+
+async function verificaSenha(senha, senhaHash) {
+    const senhaVerificada = await bcrypt.compare(senha, senhaHash);
+    if (!senhaVerificada) {
+        throw new InvalidArgumentError('Usuário ou senha incorreto!');
+    }
+}
+
+passport.use(
+    new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'senha',
+        session: false,
+    }, async (email, senha, done) => {
+        try {
+            const usuario = await Usuario.buscaPorEmail(email);
+            verificaUsuario(usuario);
+            await verificaSenha(senha, usuario.senhaHash);
+
+            done(null, usuario);
+        } catch (error) {
+            done(error);
+        }
+    })
+)
